7f7ffc7b04b058f81de9c9c3d35e47e52163c91a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-31 13:31:15 -0700

    ok

---
+++ src/_05_greeter/Greeter.java
@@ -0,0 +1,11 @@
+package _05_greeter;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Hey, what's your name?");
+		JOptionPane.showMessageDialog(null, "Cool name, I'm Khanh.");
+	}
+
+}
